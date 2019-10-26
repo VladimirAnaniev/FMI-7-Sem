7e74dd37c7b891d0ea2119096ff79ef3ec0c4f8e
vananiev <vananiev@vmware.com>
2019-10-26 14:09:41 +0300

    Cleanup beam search

--- AI/hw2/src/fmi/ai/BeamSearch.java
+++ AI/hw2/src/fmi/ai/BeamSearch.java
@@ -1,20 +1,20 @@
 package fmi.ai;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.intellij.util.containers.HashSetQueue;
+
 public class BeamSearch {
 
     public static int findShortestPath(BoardState initial, int n) {
-        Set<BoardState> closed = new HashSet<>();
-        Queue<BoardState> open = new PriorityQueue<>();
+        Set<BoardState> visited = new HashSet<>();
+        Queue<BoardState> open = new HashSetQueue<>();
         open.add(initial);
 
         while (!open.isEmpty()) {
@@ -27,29 +27,14 @@ public class BeamSearch {
             List<BoardState> nextStates = getNextStates(current, n);
 
             nextStates.stream()
-                    .filter(state -> !wasBetterStateClosed(closed, state))
-                    .filter(state -> !isBetterStateOpen(open, state))
-                    .forEach(state -> addState(open, state));
+                    .filter(state -> !visited.contains(state))
+                    .filter(state -> !open.contains(state))
+                    .forEach(open::add);
 
-            closed.add(current);
+            visited.add(current);
         }
 
         return -1;
-    }
-
-    private static boolean wasBetterStateClosed(Set<BoardState> closed, BoardState current) {
-        return closed.stream().anyMatch(
-                closedState -> closedState.equals(current) && closedState.getHeuristic() <= current.getHeuristic());
-    }
-
-    private static boolean isBetterStateOpen(Queue<BoardState> open, BoardState state) {
-        return open.stream().anyMatch(openState -> openState.equals(state) && openState.getMoves() <= state.getMoves());
-    }
-
-    private static void addState(Queue<BoardState> open, BoardState state) {
-        // Remove old sub-optimal state if exists
-        open.remove(state);
-        open.add(state);
     }
 
     private static List<BoardState> getNextStates(BoardState current, int n) {
